70c486e257c521954f36aecd1d385c00ea18b657
Adhithyan Kalaivanan <k.adhithyan@gmail.com>
2023-04-17 18:09:18 +0200

    the great refactoring

--- experiment1.py
+++ experiment1.py
@@ -1,121 +1,120 @@
 import numpy as np
 import torch
-import os
 
 from architecture.MLP import FCNet, train
 from permute import permute_align
 from utils import (
+    evaluate,
     get_data,
+    interpolation_losses,
     normalize_weights,
-    save_loss_stats,
-    plot_results,
+    loss_barrier,
 )
 
 # Set the device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # datasets
-datasets = ["moons", "gaussian"]
-n_samples = [128, 256, 512]
-n_widths = [4, 32, 1024]
+datasets = "gaussian"
+n_samples = 512
+n_widths = [3, 4, 5, 6, 12]
 
-for dataset in datasets:
-    for n_sample in n_samples:
-        for n_width in n_widths:
-            # print the experiment
-            print(f"Dataset: {dataset}, n_samples: {n_sample}, width: {n_width}")
-            # Load the data
-            train_loader, test_loader = get_data(name=dataset, n_samples=n_sample)
+for n_width in n_widths:
+    # Load the data
+    train_loader, test_loader = get_data(name=datasets, n_samples=n_samples)
 
-            # config
-            num_models = 50
-            width = n_width
-            depth = 1
-            epochs = 100
+    # config
+    num_models = 50
+    width = n_width
+    depth = 1
+    epochs = 100
 
-            # Define and train many models
-            models = []
-            for i in range(num_models):
-                model = FCNet(input_size=2, width=width, depth=depth, output_size=1)
-                train(
-                    model,
-                    train_loader,
-                    epochs=epochs,
-                    lr=0.001,
-                    model_name=f"{dataset}/model_s{n_sample}_w{width}_d{depth}_{i}",
+    # data structure to store losses and accuracies
+    logs = np.zeros((num_models, 4))
+
+    # data structure to store interpolation losses
+    interpolation_losses = np.zeros((num_models, num_models))
+
+    # data structure to store loss barriers
+    loss_barriers = np.zeros((num_models, num_models))
+
+    # Define and train many models
+    models = []
+    for i in range(num_models):
+        model = FCNet(input_size=2, width=width, depth=depth, output_size=1)
+        train(
+            model,
+            train_loader,
+            epochs=epochs,
+            lr=0.001,
+            model_name=f"{datasets}/model_s{n_samples}_w{width}_d{depth}_{i}",
+        )
+        models.append(model)
+
+        # compute train loss, test loss, train accuracy, test accuracy
+        # log the results
+        model.eval()
+
+        train_loss, train_acc = evaluate(model, train_loader)
+        test_loss, test_acc = evaluate(model, test_loader)
+
+        logs[i, 0] = train_loss
+        logs[i, 1] = test_loss
+        logs[i, 2] = train_acc
+        logs[i, 3] = test_acc
+
+        # save the log
+        np.save(
+            f"logs/sigmoid/{datasets}/logs_s{n_samples}_w{width}_d{depth}_{i}", logs
+        )
+
+    # pick index of model with lowest train loss as reference
+    reference_model_idx = np.argmin(logs[:, 0])
+
+    # normalize weights of reference model
+    models[reference_model_idx] = normalize_weights(models[reference_model_idx])
+
+    # align all other models to this reference model
+    for i in range(num_models):
+        if i == reference_model_idx:
+            continue
+        models[i] = normalize_weights(models[i])
+
+        models[reference_model_idx].eval().to(device)
+        models[i].eval().to(device)
+
+        models[i] = permute_align(
+            models[i],
+            models[reference_model_idx],
+            train_loader,
+            epochs=20,
+            device=device,
+        )
+
+    # compute interpolation loss for each pair of models
+    # log the results
+    for i in range(num_models):
+        for j in range(num_models):
+            if i == j:
+                continue
+            if i > j:
+                interpolation_losses[i, j] = interpolation_losses[j, i]
+                loss_barriers[i, j] = loss_barriers[j, i]
+                continue
+            if i < j:
+                interpolation_losses[i, j] = interpolation_losses(
+                    models[i], models[j], train_loader
                 )
-                models.append(model)
+                loss_barriers[i, j] = loss_barrier(interpolation_losses[i, j])
 
-            # or load the models from disk
-            models = []
-            for i in range(num_models):
-                model = FCNet(input_size=2, width=width, depth=depth, output_size=1)
-                model.load_state_dict(
-                    torch.load(
-                        f"models/sigmoid/{dataset}/model_s{n_sample}_w{width}_d{depth}_{i}.pth"
-                    )
-                )
-                models.append(model)
+    # save the interpolation losses
+    np.save(
+        f"logs/sigmoid/{datasets}/interpolation_losses_s{n_samples}_w{width}_d{depth}",
+        interpolation_losses,
+    )
 
-            barriers_perm = dict()
-            barriers_scale_perm = dict()
-
-            for i in range(num_models):
-                for j in range(i + 1, num_models):
-                    models[i].eval().to(device)
-                    models[j].eval().to(device)
-
-                    # align the models
-                    pi_model = permute_align(
-                        models[i], models[j], train_loader, epochs=20, device=device
-                    )
-                    torch.save(
-                        pi_model,
-                        f"models/sigmoid/{dataset}/pi_model{i}_model{j}_s{n_sample}_w{width}_d{depth}.pth",
-                    )
-
-                    # store the loss barriers
-                    barriers_perm[(i, j)] = plot_results(
-                        pi_model,
-                        models[j],
-                        train_loader,
-                        test_loader,
-                        name=f"{dataset}_model{i}_model{j}_unscaled_perm",
-                        save=False,
-                    )
-
-                    # rescale the models
-                    modeli_rescaled = normalize_weights(models[i])
-                    modelj_rescaled = normalize_weights(models[j])
-
-                    # align the rescaled models
-                    pi_model = permute_align(
-                        modeli_rescaled,
-                        modelj_rescaled,
-                        train_loader,
-                        epochs=20,
-                        device=device,
-                    )
-                    torch.save(
-                        pi_model,
-                        f"models/sigmoid/{dataset}/pi_model{i}_model{j}_s{n_sample}_w{width}_d{depth}_scale.pth",
-                    )
-
-                    # store the loss barriers
-                    barriers_scale_perm[(i, j)] = plot_results(
-                        pi_model,
-                        modelj_rescaled,
-                        train_loader,
-                        test_loader,
-                        name=f"{dataset}_model{i}_model{j}_rescaled_perm",
-                        save=False,
-                    )
-
-            # save loss statistics
-            save_loss_stats(
-                barriers_perm,
-                barriers_scale_perm,
-                num_models=num_models,
-                name=f"{dataset}/s{n_sample}_w{width}",
-            )
-print("Done!")
+    # save the loss barriers
+    np.save(
+        f"logs/sigmoid/{datasets}/loss_barriers_s{n_samples}_w{width}_d{depth}",
+        loss_barriers,
+    )
--- utils.py
+++ utils.py
@@ -180,28 +180,6 @@ def get_data(name: str = "blobs", n_samples=512) -> tuple:
     return train_loader, test_loader
 
 
-def interpolate_models(model1, model2, alpha):
-    """
-    Interpolate between two models for a given alpha
-    """
-    # get the state dicts
-    state_dict1 = model1.state_dict()
-    state_dict2 = model2.state_dict()
-
-    # create a new state dict
-    new_state_dict = OrderedDict()
-
-    # interpolate the weights and biases
-    for k in state_dict1:
-        new_state_dict[k] = alpha * state_dict1[k] + (1 - alpha) * state_dict2[k]
-
-    # load the new state dict into a new model
-    new_model = copy.deepcopy(model1)
-    new_model.load_state_dict(new_state_dict)
-
-    return new_model
-
-
 # account for scale symmetry (TO DO: account for normalization layers)
 def normalize_weights(model):
     """
@@ -258,15 +236,18 @@ def normalize_weights(model):
     return new_model
 
 
-def compute_loss(model, loader):
+# evaluate the loss and accuracy given a model and loader
+def evaluate(model, loader):
     """
-    Compute the loss of a model on a dataset
+    Evaluate the loss and accuracy given a model and loader
     """
     # initialize the loss
     loss = 0
+    # initialize the accuracy
+    accuracy = 0
+
     # for each batch
     for X, y in loader:
-        y = y.unsqueeze(1).float()
         # move the data to the device
         X = X.to(device)
         y = y.to(device)
@@ -274,19 +255,15 @@ def compute_loss(model, loader):
         # compute the loss
         loss += F.binary_cross_entropy_with_logits(model(X), y).item()
 
-    # return the loss
-    return loss / len(loader)
+        # compute the accuracy
+        accuracy += (model(X).argmax(dim=1) == y).float().mean().item()
+
+    # return the loss and accuracy
+    return loss / len(loader), accuracy / len(loader)
 
 
-# plot results - get both loss curves and decision boundaries in one plot
-# as 3x4 grid, with 1 subplot for loss curves and others for decision boundaries
-def plot_results(
-    model1, model2, train_loader, test_loader, name, num_points=11, save=False
-):
-    """
-    Plot results - get both loss curves and decision boundaries in one plot
-    as 3x4 grid, with 1 subplot for loss curves and others for decision boundaries
-    """
+# given 2 models and loaders, return a np array of interpolation losses
+def interpolation_losses(model1, model2, loader, num_points=11):
     # get the state dicts
     state_dict1 = model1.state_dict()
     state_dict2 = model2.state_dict()
@@ -295,19 +272,17 @@ def plot_results(
     new_state_dict = OrderedDict()
 
     alphas = np.linspace(0, 1, num_points)
-    train_losses = []
-    test_losses = []
-
-    # create a figure
-    fig, axes = plt.subplots(3, 4, figsize=(20, 15))
-
-    i, j = 0, 1
+    losses = np.zeros(
+        num_points,
+    )
 
     # for each alpha
-    for alpha in alphas:
+    for idx in range(len(alphas)):
         # interpolate the weights and biases
         for k in state_dict1:
-            new_state_dict[k] = alpha * state_dict1[k] + (1 - alpha) * state_dict2[k]
+            new_state_dict[k] = (
+                alphas[idx] * state_dict1[k] + (1 - alphas[idx]) * state_dict2[k]
+            )
 
         # load the new state dict into a new model
         new_model = copy.deepcopy(model1)
@@ -316,117 +291,17 @@ def plot_results(
         new_model.eval()
 
         # compute the loss
-        train_loss = compute_loss(new_model, train_loader)
-        test_loss = compute_loss(new_model, test_loader)
+        loss, _ = evaluate(new_model, loader)
 
-        # append the loss
-        train_losses.append(train_loss)
-        test_losses.append(test_loss)
+        # store the loss
+        losses[idx] = loss
 
-        if save == True:
-            # get the limits of grid from the data
-            X, y = next(iter(test_loader))
-            x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
-            y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
-
-            # create a grid of points
-            xx, yy = np.meshgrid(
-                np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100)
-            )
-
-            # model is trained with softmax, so apply to output and plot contours of probabilities
-            Z = F.softmax(
-                new_model(torch.Tensor(np.c_[xx.ravel(), yy.ravel()]).to(device)), dim=1
-            )
-
-            # plot the contours in pleasant grey scale
-            Z = Z[:, 1].cpu().detach().numpy()
-            Z = Z.reshape(xx.shape)
-            axes[i, j].contourf(xx, yy, Z, cmap=plt.cm.Greys)
-
-            # plot the data
-            for k in range(2):
-                axes[i, j].scatter(
-                    X[y == k, 0].cpu().detach().numpy(),
-                    X[y == k, 1].cpu().detach().numpy(),
-                    c="C{}".format(k),
-                    alpha=0.5,
-                )
-
-            # if model has only one hidden layer, plot each neuron in different color
-            if len(new_model.layers) == 2:
-                # get the weights and biases
-                w = new_model.layers[0].weight.cpu().detach().numpy()
-                b = new_model.layers[0].bias.cpu().detach().numpy()
-
-                # for each neuron
-                for k in range(w.shape[0]):
-                    # plot the line for the neuron in different color
-                    axes[i, j].plot(
-                        np.linspace(x_min, x_max, 100),
-                        -(w[k, 0] * np.linspace(x_min, x_max, 100) + b[k]) / w[k, 1],
-                        color="C{}".format(k + 2),
-                        linestyle="--",
-                    )
-
-            # set the title (alpha upto 1 decimal place)
-            axes[i, j].set_title(r"$\alpha$ = {:.1f}".format(alpha))
-
-            # set axis limits
-            axes[i, j].set_xlim(x_min, x_max)
-            axes[i, j].set_ylim(y_min, y_max)
-
-            # increment the indices
-            j += 1
-            if j == 4:
-                i += 1
-                j = 0
-
-    if save == True:
-        # plot the loss, connect the markers, and set the color
-        axes[0, 0].plot(alphas, train_losses, color="C0", marker="o", linestyle="-")
-        axes[0, 0].plot(alphas, test_losses, color="C1", marker="o", linestyle="--")
-
-        # set the labels
-        axes[0, 0].set_xlabel(r"$\alpha$")
-        axes[0, 0].set_ylabel("Loss")
-        axes[0, 0].set_title("Loss Curves")
-
-        # set the legend
-        axes[0, 0].legend(["Train", "Test"])
-
-        # set the axis limits
-        axes[0, 0].set_xlim(0, 1)
-        axes[0, 0].set_ylim(0, 4)
-
-        # main title
-        fig.suptitle(f"Interpolation: {name}")
-
-        # save the plot
-        plt.savefig("plots/{}.png".format(name))
-
-    # close the plot
-    plt.close()
-
-    # loss statistics
-    barriers = loss_barriers(train_losses, test_losses)
-
-    # return the stats
-    return barriers
+    return
 
 
-# loss statistics
-def loss_barriers(train_losses, test_losses):
-    barriers = OrderedDict()
+# given a np array of interpolation losses, return the loss barrier
+def loss_barrier(losses):
+    # compute the loss barrier as max(losses - linspace(losses[0], losses[-1], len(losses)))
+    loss_barrier = np.max(losses - np.linspace(losses[0], losses[-1], len(losses)))
 
-    # get loss barrier
-    # subtract linear interpolation of end points from losses
-    # and get the max of the resulting array
-    barriers["train"] = max(
-        train_losses - np.linspace(train_losses[0], train_losses[-1], len(train_losses))
-    )
-    barriers["test"] = max(
-        test_losses - np.linspace(test_losses[0], test_losses[-1], len(test_losses))
-    )
-
-    return barriers
+    return loss_barrier
